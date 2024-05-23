0476e54f8f973921d47e77a0cec673f4abf51fe2
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-05-23 16:26:19 +0300

    refactor: rework calc fn

--- internal/usecase/calculating_exchange.go
+++ internal/usecase/calculating_exchange.go
@@ -64,30 +64,38 @@ var ErrNotEnoughNecessaryBanknotes = errors.New("there are not enough necessary 
 
 // banknotes sorted descending order
 func calculate(banknotes []int, amount int) ([]CalculatingExchangeOption, error) {
-	// sort
+	// sort:
+
 	sorted := make([]int, len(banknotes))
 	copy(sorted, banknotes)
 	slices.Sort(sorted)
 	banknotes = sorted
 
-	// process
+	// process:
+
 	if len(banknotes) == 0 {
 		return nil, nil
 	}
 	if amount%banknotes[0] != 0 {
 		return nil, ErrNotEnoughNecessaryBanknotes
 	}
-	var initial []int // contains only min banknotes
-	for i := 0; i < amount/banknotes[0]; i++ {
-		initial = append(initial, banknotes[0])
-	}
+
 	var result []CalculatingExchangeOption
-	result = append(result, initial)
-	if len(banknotes) == 1 {
-		return result, nil
-	}
-	for i := 1; i < len(banknotes); i++ {
-		for _, b := range banknotes[i:] {
+	for _, maxb := range banknotes[1:] {
+		//if amount-banknotes[i] == 0 {
+		//	return append(result, ), nil
+		//} else if amount-banknotes[i] < 0 {
+		//
+		//}
+		var initial []int // contains only min banknotes
+		for i := 0; i < amount/maxb; i++ {
+			initial = append(initial, maxb)
+		}
+		result = append(result, initial)
+		if len(banknotes) == 1 {
+			return result, nil
+		}
+		for _, b := range banknotes[1:] {
 			curOpt := make([]int, len(initial))
 			copy(curOpt, initial)
 			for {
@@ -109,3 +117,127 @@ func calculate(banknotes []int, amount int) ([]CalculatingExchangeOption, error)
 	}
 	return result, nil
 }
+
+// banknotes sorted descending order
+func calculateV2(banknotes []int, amount int) ([]CalculatingExchangeOption, error) {
+	// sort:
+
+	sorted := make([]int, len(banknotes))
+	copy(sorted, banknotes)
+	slices.Sort(sorted)
+	slices.Reverse(sorted)
+	banknotes = sorted
+
+	// upper bound
+
+	for i, b := range banknotes {
+		if b <= amount {
+			banknotes = banknotes[i:]
+			break
+		}
+	}
+
+	// ex cases:
+
+	if len(banknotes) == 0 {
+		return nil, nil
+	}
+
+	if amount%banknotes[len(banknotes)-1] != 0 {
+		println(banknotes[len(banknotes)-1])
+		return nil, ErrNotEnoughNecessaryBanknotes
+	}
+
+	// process:
+
+	var result []CalculatingExchangeOption
+	for iii := range banknotes {
+		for ii := range banknotes[iii:] {
+			remAmount := amount
+			var curOpt []int
+			for _, b := range banknotes[ii:] { // брать начиная с initialB
+				for remAmount >= b { // сколько раз поместить сумму
+					curOpt = append(curOpt, b)
+					remAmount -= b
+				}
+			}
+			if remAmount == 0 {
+				result = append(result, curOpt)
+				tmpOpts := curOpt
+				curOpt = nil
+				remAmount = 0
+				for i := 0; i < len(tmpOpts); i++ {
+					t
+				}
+			}
+		}
+
+	}
+	return result, nil
+}
+
+//
+//// banknotes sorted descending order
+//func calculateV3(banknotes []int, amount int) ([]CalculatingExchangeOption, error) {
+//	// sort:
+//	sorted := make([]int, len(banknotes))
+//	copy(sorted, banknotes)
+//	slices.Sort(sorted)
+//	banknotes = sorted
+//
+//	// upper bound:
+//	slices.Reverse(banknotes)
+//	for i, b := range banknotes {
+//		if b <= amount {
+//			banknotes = banknotes[i:]
+//			break
+//		}
+//	}
+//	slices.Reverse(banknotes)
+//
+//	// ex cases:
+//	if len(banknotes) == 0 {
+//		return nil, nil
+//	}
+//	if amount%banknotes[0] != 0 {
+//		return nil, ErrNotEnoughNecessaryBanknotes
+//	}
+//
+//	// process:
+//
+//	return result, nil
+//}
+//func processV3(banknotes []int, note int, amount int)  {
+//
+//}
+//
+//func appendIfMay(banknotes []int, note int, amount int) ([]int, bool) {
+//	currentAmount := 0
+//	for _, b := range banknotes {
+//		currentAmount += b
+//	}
+//	if currentAmount+am {
+//
+//	}
+//
+//
+//
+//
+//
+//
+//	currentAmount := 0
+//	banknotes = append(banknotes, note)
+//	slices.Sort(banknotes)
+//	slices.Reverse(banknotes)
+//	for _, b := range banknotes {
+//		currentAmount += b
+//	}
+//	for currentAmount > amount  {
+//		if len(banknotes) != 0 {
+//			banknotes
+//		} else {
+//			return nil, false
+//		}
+//	}
+//	return banknotes, true
+//}
--- internal/usecase/calculating_exchange_test.go
+++ internal/usecase/calculating_exchange_test.go
@@ -14,12 +14,12 @@ func TestCalculateExchangeOptions(t *testing.T) {
 		wantExchange []CalculatingExchangeOption
 		wantErr      error
 	}{
-		{
-			banknotes:    []int{50},
-			amount:       401,
-			wantExchange: nil,
-			wantErr:      ErrNotEnoughNecessaryBanknotes,
-		},
+		//{
+		//	banknotes:    []int{50},
+		//	amount:       401,
+		//	wantExchange: nil,
+		//	wantErr:      ErrNotEnoughNecessaryBanknotes,
+		//},
 		{
 			banknotes: []int{5000, 2000, 1000, 500, 200, 100, 50},
 			amount:    400,
@@ -40,7 +40,7 @@ func TestCalculateExchangeOptions(t *testing.T) {
 	// The execution loop
 	for i, tt := range tests {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
-			ans, err := calculate(tt.banknotes, tt.amount)
+			ans, err := calculateV2(tt.banknotes, tt.amount)
 			if !errors.Is(err, tt.wantErr) {
 				t.Errorf("Got error:\n %v\nWant error:\n %v", ans, tt.wantExchange)
 			} else if !reflect.DeepEqual(ans, tt.wantExchange) {
