260c048a9d0144d2713f032a9f704410945cfebb
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-05-25 20:46:03 +0300

    refactor: rework calc exchange fn, rm error from recursive fn and add expression for correctly return error

--- internal/usecase/calculating_exchange.go
+++ internal/usecase/calculating_exchange.go
@@ -16,16 +16,15 @@ type CalculatingExchangeInput struct {
 type CalculatingExchange struct{}
 
 func (c *CalculatingExchange) Handle(input CalculatingExchangeInput) (CalculatingExchangeOutput, error) {
-	// 1. validate unprocessed if needed
-	// 2. process
-	ex, err := calculateExchange(input.Banknotes, input.Amount)
+	// 1. validate unprocessed if needed: -
+	// 2. process:
+	exchange, err := calculateExchange(input.Banknotes, input.Amount)
 	if err != nil {
 		return CalculatingExchangeOutput{}, err
 	}
-	out := CalculatingExchangeOutput{
-		ExchangeOptions: ex,
-	}
-	return out, nil
+	return CalculatingExchangeOutput{
+		ExchangeOptions: exchange,
+	}, nil
 }
 
 var ErrNotEnoughNecessaryBanknotes = errors.New("ErrNotEnoughNecessaryBanknotes")
@@ -33,36 +32,43 @@ var ErrNotEnoughNecessaryBanknotes = errors.New("ErrNotEnoughNecessaryBanknotes"
 // thx to ChatGPT (based on https://www.geeksforgeeks.org/coin-change-dp-7/) for help:
 
 func calculateExchange(banknotes []int, amount int) ([][]int, error) {
-	var fn func(banknotes []int, n int, amount int) ([][]int, error)
-	fn = func(banknotes []int, n, amount int) ([][]int, error) {
+	if len(banknotes) == 0 {
+		if amount > 0 {
+			return nil, ErrNotEnoughNecessaryBanknotes
+		}
+		return nil, nil
+	} else if amount == 0 {
+		return nil, nil
+	}
+
+	var fn func(banknotes []int, n int, amount int) [][]int
+	fn = func(banknotes []int, n, amount int) [][]int {
 		if amount == 0 {
-			return [][]int{{}}, nil // Базовый случай: пустой вариант
+			return [][]int{{}} // Базовый случай: пустой вариант
 		}
 		if amount < 0 {
-			return nil, ErrNotEnoughNecessaryBanknotes // Невозможный случай: возвращаем пустой список вариантов
+			return nil // Невозможный случай: возвращаем пустой список вариантов
 		}
 		if n <= 0 {
-			return nil, nil // Невозможный случай: возвращаем пустой список вариантов
+			return nil // Невозможный случай: возвращаем пустой список вариантов
 		}
 
 		// Рассматриваем два случая:
 		// 1. Включаем монету banknotes[n-1] в вариант
-		variants1, err := fn(banknotes, n, amount-banknotes[n-1])
-		if err != nil {
-			return nil, err
-		}
+		variants1 := fn(banknotes, n, amount-banknotes[n-1])
 		for i := range variants1 {
 			variants1[i] = append(variants1[i], banknotes[n-1]) // Добавляем монету в вариант
 		}
 
 		// 2. Не включаем монету banknotes[n-1] в вариант
-		variants2, err := fn(banknotes, n-1, amount)
-		if err != nil {
-			return nil, err
-		}
+		variants2 := fn(banknotes, n-1, amount)
 
 		// Объединяем варианты из обоих случаев
-		return append(variants1, variants2...), nil
+		return append(variants1, variants2...)
 	}
-	return fn(banknotes, len(banknotes), amount)
+	exchanges := fn(banknotes, len(banknotes), amount)
+	if len(banknotes) > 0 && len(exchanges) == 0 {
+		return nil, ErrNotEnoughNecessaryBanknotes
+	}
+	return exchanges, nil
 }
