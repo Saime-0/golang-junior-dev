aeace12d70c299eb7ff5b289516cd82dc3c3994c
Saime Dejuwyu <acc.saime.d@gmail.com>
2024-05-25 20:57:58 +0300

    refactor: change handling components notifications

--- internal/app/app.go
+++ internal/app/app.go
@@ -28,14 +28,25 @@ func Start(ctx context.Context, cfg *config.Config) error {
 		<-httpServer.Done()
 		wg.Done()
 	}()
-
+	errChan := make(chan error)
+	go func() {
+		var err error
+		select { // read notifications from components:
+		case err = <-httpServer.Notify():
+		}
+		select { // send to err chan
+		case errChan <- fmt.Errorf("app - Run - httpServer.Notify: %w", err):
+		default: // <- non blocking
+		}
+		close(errChan)
+	}()
 	select {
 	case <-ctx.Done():
 		logrus.Info("app - Run - receive ctx.Done, wait when components stop the work")
 		wg.Wait()
 		logrus.Info("app - Run - components done the work")
 		return nil
-	case err := <-httpServer.Notify():
-		return fmt.Errorf("app - Run - httpServer.Notify: %w", err)
+	case err := <-errChan:
+		return err
 	}
 }
